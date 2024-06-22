1aa778d36e1683db144d9361b46ec0675cf27d7c
Karthick Sivakumar <karthick@globalwavenet.com>
2024-06-22 10:08:23 +0530

    Included power and modulus functions

--- calculator/operations.py
+++ calculator/operations.py
@@ -17,3 +17,13 @@ def division(number_1, number_2):
     # return number_1 / number_2
     print('{} / {} = '.format(number_1, number_2))
     print(number_1 / number_2)
+    
+def power(number_1, number_2):
+    # return number_1 / number_2
+    print('{} ** {} = '.format(number_1, number_2))
+    print(number_1 ** number_2)
+    
+def modulus(number_1, number_2):
+    # return number_1 / number_2
+    print('{} % {} = '.format(number_1, number_2))
+    print(number_1 % number_2)
