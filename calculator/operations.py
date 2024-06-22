ec848f056ecf113ca3d31c1b69c4211ebe2434cd
Karthick Sivakumar <karthick@globalwavenet.com>
2024-06-22 17:20:05 +0530

    Added new two features absolute, inverse function and removed the print lines and return the values

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,23 +1,30 @@
 def addition(number_1, number_2):
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+    return number_1 + number_2
+
 
 def subtraction(number_1, number_2):
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
-    
+    return number_1 - number_2
+
+
 def multiplication(number_1, number_2):
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    return number_1 * number_2
+
 
 def division(number_1, number_2):
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
-    
+    return number_1 / number_2
+
+
 def power(number_1, number_2):
-    print('{} ** {} = '.format(number_1, number_2))
-    print(number_1 ** number_2)
-    
+    return number_1 ** number_2
+
+
 def modulus(number_1, number_2):
-    print('{} % {} = '.format(number_1, number_2))
-    print(number_1 % number_2)
+    return number_1 % number_2
+
+
+def absolute(number):
+    return abs(number)
+
+
+def inverse(number):
+    return 1 / number
