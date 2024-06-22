6a61147cb092e55eb8b11b1b0dd139c8175db375
Karthick Sivakumar <karthick@globalwavenet.com>
2024-06-22 18:59:33 +0530

    ZeroDivisionError was handled

--- calculator/operations.py
+++ calculator/operations.py
@@ -11,7 +11,10 @@ def multiplication(number_1, number_2):
 
 
 def division(number_1, number_2):
-    return number_1 / number_2
+    try:
+        return number_1 / number_2
+    except ZeroDivisionError:
+        return "Error: division by zero"
 
 
 def power(number_1, number_2):
@@ -19,7 +22,10 @@ def power(number_1, number_2):
 
 
 def modulus(number_1, number_2):
-    return number_1 % number_2
+    try:
+        return number_1 % number_2
+    except ZeroDivisionError:
+        return "Error: division by zero"
 
 
 def absolute(number):
@@ -27,4 +33,7 @@ def absolute(number):
 
 
 def inverse(number):
-    return 1 / number
+    try:
+        return 1 % number
+    except ZeroDivisionError:
+        return "Error: division by zero"
