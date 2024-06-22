b72f10dd5fedd87136ced2260a269c412e7c83ca
Karthick Sivakumar <karthick@globalwavenet.com>
2024-06-22 23:21:31 +0530

    Updated the README file and included three new features such as factorial, exponential and log base e.

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,3 +1,6 @@
+import math
+
+
 def addition(number_1, number_2):
     return number_1 + number_2
 
@@ -14,7 +17,7 @@ def division(number_1, number_2):
     try:
         return number_1 / number_2
     except ZeroDivisionError:
-        return "Error: division by zero"
+        return "Error: denominator is zero"
 
 
 def power(number_1, number_2):
@@ -25,7 +28,7 @@ def modulus(number_1, number_2):
     try:
         return number_1 % number_2
     except ZeroDivisionError:
-        return "Error: division by zero"
+        return "Error: denominator is zero"
 
 
 def absolute(number):
@@ -36,4 +39,29 @@ def inverse(number):
     try:
         return 1 / number
     except ZeroDivisionError:
-        return "Error: division by zero"
+        return "Error: denominator is zero"
+
+
+def factorial(number):
+    if number == 0:
+        return 1
+    elif number == 1:
+        return 1
+    elif number > 0 and number % 1 == 0:
+        Factorial = 1
+        while number > 1:
+            Factorial *= number
+            number -= 1
+        return Factorial
+    else:
+        return "Error: Negative integer or float value is passed"
+
+
+def exponential(number):
+    return math.exp(number)
+
+def log_base_e(number):
+    try:
+        return math.log(number)
+    except ValueError as e:
+        return f"Error: {e}"
